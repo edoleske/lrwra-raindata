a115d3882458311584b9838ae73d8b373588c534
Ethan O'Leske <ethan.o'leske@lrwra.com>
2023-05-02 10:16:07 -0500

    Adding side drawer to layout

--- src/components/layout/AppNavbar.tsx
+++ src/components/layout/AppNavbar.tsx
@@ -1,19 +1,41 @@
 import Image from "next/image";
-import Link from "next/link";
+import { MdMenu, MdClose } from "react-icons/md";
 
-const AppNavbar = () => {
+interface AppNavbarProps {
+  menuOpen: boolean;
+  onMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AppNavbar = ({ menuOpen, onMenuOpen }: AppNavbarProps) => {
   return (
     <div className="navbar bg-primary text-primary-content">
-      <Link href="https://lrwra.com/" className="ml-2">
+      <div className="flex-none lg:hidden">
+        <label className="swap-rotate swap btn-ghost btn-circle btn">
+          <input
+            type="checkbox"
+            checked={menuOpen}
+            onChange={(e) => onMenuOpen(e.target.checked)}
+          />
+          <MdMenu className="swap-off fill-current" size={26} />
+          <MdClose className="swap-on fill-current" size={26} />
+        </label>
+      </div>
+      <a
+        href="https://www.lrwra.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="ml-2"
+      >
         <Image
           src="/LRWRA_LogoColor.png"
           width={28}
           height={32}
+          style={{ height: "auto" }}
           alt="Little Rock Water Reclamation Authority Logo"
         />
-      </Link>
+      </a>
       <div className="flex-1">
-        <span className="ml-3 text-2xl font-bold">Rain Data</span>
+        <span className="ml-4 text-2xl font-bold">Rain Data</span>
       </div>
     </div>
   );
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 import AppNavbar from "~/components/layout/AppNavbar";
 
 interface LayoutProps {
@@ -5,10 +8,59 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const router = useRouter();
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
     <>
-      <AppNavbar />
-      <main className="height-minus-appbar">{children}</main>
+      <AppNavbar menuOpen={drawerOpen} onMenuOpen={setDrawerOpen} />
+      <div className="height-minus-appbar drawer-mobile drawer">
+        <input
+          id="app-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+          checked={drawerOpen}
+          onChange={(e) => setDrawerOpen(e.target.checked)}
+        />
+        <div className="drawer-content">
+          <main className="height-minus-appbar">{children}</main>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="app-drawer" className="drawer-overlay"></label>
+          <ul className="menu w-60 overflow-y-auto bg-base-300 p-4 font-semibold text-base-content">
+            <li>
+              <Link href="/" className={router.asPath === "/" ? "active" : ""}>
+                Gauge Map
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/testdata"
+                className={router.asPath === "/testdata" ? "active" : ""}
+              >
+                Table
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/graph"
+                className={router.asPath === "/graph" ? "active" : ""}
+              >
+                Graph
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/download"
+                className={router.asPath === "/download" ? "active" : ""}
+              >
+                Download
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
